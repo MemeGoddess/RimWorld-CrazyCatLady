deee944e6e2442e97c0b922a17638a2fa56e59c8
Lana Avery <memekoten@gmail.com>
2026-05-31 19:40:44 +1000

    Cleanup

--- Source/Crazy_Cat_Lady.cs
+++ Source/Crazy_Cat_Lady.cs
@@ -18,29 +18,12 @@ namespace Crazy_Cat_Lady
 
 	public class Mod : Verse.Mod
 	{
-		//public static Settings settings;
 		public Mod(ModContentPack content) : base(content)
 		{
-			// initialize settings
-			// settings = GetSettings<Settings>();
-#if DEBUG
-			Harmony.DEBUG = true;
-#endif
 
 			Harmony harmony = new Harmony("Uuugggg.rimworld.Crazy_Cat_Lady.main");
 
 			harmony.PatchAll();
 		}
-
-//		public override void DoSettingsWindowContents(Rect inRect)
-//		{
-//			base.DoSettingsWindowContents(inRect);
-//			settings.DoWindowContents(inRect);
-//		}
-//
-//		public override string SettingsCategory()
-//		{
-//			return "Crazy Cat Lady";
-//		}
 	}
 }
